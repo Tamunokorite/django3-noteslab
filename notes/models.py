7b4900192b618ab9da517cb84ae9a239b2e104ff
Tamunokorite Briggs <obriggs03@gmail.com>
2021-09-19 14:16:47 +0100

    Created Note Model

--- notes/models.py
+++ notes/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
+class Note(models.Model):
+    title = models.CharField(max_length=150)
+    content = models.TextField(blank=True)
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=False, auto_now_add=False)
+    deleted = models.BooleanField(default=False)
+
+    class Meta:
+        verbose_name = ("Note")
+        verbose_name_plural = ("Notes")
+
+    def __str__(self):
+        return self.name
--- notes_app/urls.py
+++ notes_app/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from decouple import config
+from notes import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path(config('ADMIN_SITE_URL'), admin.site.urls),
+
+    path('', views.index, name='index')
 ]
