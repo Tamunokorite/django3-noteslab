e9c399cd9e2acde33dd3bd1a0c41171d7a2d7a9f
Tamunokorite Briggs <obriggs03@gmail.com>
2021-09-20 00:10:07 +0100

    Added heroku configuration

--- notes_app/prod_settings.py
+++ notes_app/prod_settings.py
@@ -17,7 +17,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config("DEBUG", default=False)
 
-ALLOWED_HOSTS = ["https://noteslab.herokuapp.com/"]
+ALLOWED_HOSTS = ["noteslab.herokuapp.com"]
 
 
 # Application definition
