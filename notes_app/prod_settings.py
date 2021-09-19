b4492041f1785fb628a457cb8ef840121e651e91
Tamunokorite Briggs <obriggs03@gmail.com>
2021-09-20 00:01:32 +0100

    Added heroku configuration

--- notes_app/prod_settings.py
+++ notes_app/prod_settings.py
@@ -109,6 +109,8 @@ LOGIN_URL = '/login/'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
+STATIC_ROOT = BASE_DIR / 'static'
+
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
